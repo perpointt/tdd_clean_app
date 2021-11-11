b53fc44663e4ac0d9303b84d6491587a73d0a690
Maxim Ilyshchenko <per.pointt@gmail.com>
2021-11-11 17:20:43 +0300

    lesson 5

---
+++ lib/features/number_trivia/data/datasourses/number_trivia_local_data_source.dart
@@ -0,0 +1,11 @@
+import 'package:tdd_clean_app/features/number_trivia/data/models/number_trivia_model.dart';
+
+abstract class NumberTriviaLocalDataSource {
+  /// Gets the cached [NumberTriviaModel] which was gotten the last time
+  /// the user had an internet connection.
+  ///
+  /// Throws CacheException if no cached data is present.
+  Future<NumberTriviaModel> getLastNumberTrivia();
+
+  Future<void> cacheNumberTrivia(NumberTriviaModel traviaToCache);
+}
